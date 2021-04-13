28bef006bdfc11b6df9b49c02546d34e86f30067
Wiktor Kulikowski <scotti.pol@gmail.com>
2021-04-13 10:21:56 +0200

    Copy code with promt,and paste. Create block else if and else

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1) {
-  computerMove = 'kamień';
+    computerMove = 'kamień';
 }   else if(randomNumber == 2) {
         computerMove = 'papier';
 }   else {
@@ -13,3 +13,19 @@ if(randomNumber == 1) {
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}   else if(playerInput == '2') {
+        playerMove = 'papier';
+}   else {
+        playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
