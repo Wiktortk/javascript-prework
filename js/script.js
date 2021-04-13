f29917f0d2aca677bf6b059dce72a36d5fe08e3b
Wiktor Kulikowski <scotti.pol@gmail.com>
2021-04-13 09:59:39 +0200

    Refill blocks else if and else.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1) {
+  computerMove = 'kamień';
+}   else if(randomNumber == 2) {
+        computerMove = 'papier';
+}   else {
+        computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
