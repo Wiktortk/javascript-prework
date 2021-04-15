4f6e2d3dcd66af1b66b3aa6449a617bcaa7f16be
Wiktor Kulikowski <scotti.pol@gmail.com>
2021-04-15 21:42:30 +0200

    This commit have mistakes- need advice

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 let computerMove = getMoveName(randomNumber);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+//let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 let playerMove = getMoveName(playerInput);
 
@@ -15,3 +15,15 @@ printMessage('Mój ruch to: ' + computerMove);
 printMessage('Twój ruch to: ' + playerMove);
 
 printMessage('Wynik to : ' + displayResult(computerMove, playerMove));
+
+document.getElementById('play-rock').addEventListener('click', function() {
+    playGame(playerInput + 'Wybrałeś kamień');
+});
+
+document.getElementById('play-paper').addEventListener('click', function() {
+    playGame(playerInput + 'Wybrałeś papier');
+});
+
+document.getElementById('play-scissors').addEventListener('click', function() {
+    playGame(playerInput + 'Wybrałeś nożyce');
+});
