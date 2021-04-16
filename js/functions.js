f0023b4aa3386678c9d847166a564f05cb449995
Wiktor Kulikowski <scotti.pol@gmail.com>
2021-04-16 19:21:18 +0200

    file function.js with funkcjami. Checked - runns.

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,60 @@ function printMessage(msg) {
 function clearMessages() {
 	document.getElementById('messages').innerHTML = '';
 }
+
+function getMoveName(computerMove) {
+	if(computerMove == 1) {
+		return 'kamień';
+	} else if(computerMove == 2) {
+		return 'papier';
+	} else if(computerMove == 3) {
+		return 'nożyce'; 
+	} else{
+		return 'nieznany ruch';
+	}
+}
+
+function displayResult(computerMove, playerMove) {
+	if( computerMove == 'kamień' && playerMove == 'papier') {
+		return 'Ty wygrywasz!';
+	} else if(computerMove == 'papier' && playerMove == 'nożyce') {
+		return 'Ty wygrywasz!';
+	} else if(computerMove == 'nożyce' && playerMove == 'kamień') {
+		return 'Ty wygrywasz!';
+	} else if(playerMove == 'papier' && computerMove == 'nożyce') {
+		return 'Komputer wygrywa!';
+	} else if(playerMove == 'nożyce' && computerMove == 'kamień') {
+		return 'Komputer wygrywa!';
+	} else if(playerMove == 'kamień' && computerMove == 'papier') {
+		return 'Komputer wygrywa!';
+	} else if(playerMove == 'papier' && computerMove == 'papier') {
+		return 'Remis!';
+	} else if(playerMove == 'nożyce' && computerMove == 'nożyce') {
+		return 'Remis!';
+	} else if(playerMove == 'kamień' && computerMove == 'kamień') {
+		return 'Remis!';
+	}
+}
+
+function playGame(playerInput) {   
+    
+    clearMessages();   
+
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+    let computerMove = getMoveName(randomNumber);
+
+    //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+    let playerMove = getMoveName(playerInput);
+
+    console.log('Wylosowana liczba to: ' + randomNumber);
+
+    console.log('Gracz wpisał: ' + playerInput);
+
+    printMessage('Mój ruch to: ' + computerMove);
+        
+    printMessage('Twój ruch to: ' + playerMove);
+
+    printMessage('Wynik to : ' + displayResult(computerMove, playerMove));
+} 
